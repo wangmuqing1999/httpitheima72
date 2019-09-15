40021beb987d5c6837eaf02eb7add611973c90de
wangmuqing <1719990136@qq.com>
2019-09-15 10:55:56 +0800

    提交

---
+++ src/main/java/User.java
@@ -0,0 +1,11 @@
+public class User {
+    private String name;
+
+    public User(String name, String age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    private String age;
+
+}
